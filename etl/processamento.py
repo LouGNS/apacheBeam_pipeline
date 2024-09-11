d214cc7a14c90bda63f182e93b5ef176bf503c6e
Luiz Gustavo <lgustavo.nsilva@gmail.com>
2024-09-11 12:29:33 -0300

    processing script correction

--- etl/processamento.py
+++ etl/processamento.py
@@ -1,55 +1,25 @@
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
+import pyarrow as pa
 import csv
-import unicodedata
 from datetime import datetime
 import pytz
 
-# Função para remover acentuação e espaços em branco e padronizar em maiúsculas
-def preprocess_text(text):
-    if text:
-        text = text.strip()  # Remove espaços em branco
-        text = unicodedata.normalize('NFKD', text).encode('ASCII', 'ignore').decode('utf-8')  # Remove acentuação
-        return text.upper()  # Padroniza em maiúsculas
-    return text
-
-# Função de pré-processamento
 def preprocess_data(element):
-    # Converte todas as colunas para string e aplica preprocessamento
-    element['nome'] = preprocess_text(str(element.get('nome', '')))
-    element['cpf'] = str(element.get('cpf', ''))
-    element['email'] = preprocess_text(str(element.get('email', '')))
-    element['numeroConta'] = str(element.get('numeroConta', ''))
-    element['numeroCartao'] = str(element.get('numeroCartao', ''))
-    element['ranking'] = str(element.get('ranking', ''))
-
-    # Adiciona a coluna DT_CARGA com a data e horário atual em UTC
     element['DT_CARGA'] = datetime.now(pytz.utc).strftime('%Y-%m-%d %H:%M:%S')
 
+    # Convertendo para os tipos apropriados
+    if 'cpf' in element:
+        element['cpf'] = int(element['cpf'])
+    if 'numeroConta' in element:
+        element['numeroConta'] = int(element['numeroConta'])
+    if 'numeroCartao' in element:
+        element['numeroCartao'] = int(element['numeroCartao'])
+    
     return element
 
-# Função para definir o esquema dos dados
-def get_ranking_schema():
-    return {
-        'fields': [
-            {'name': 'cpf', 'type': 'INT64'},
-            {'name': 'numeroConta', 'type': 'INT64'},
-            {'name': 'numeroCartao', 'type': 'INT64'},
-            {'name': 'ranking', 'type': 'STRING'}
-        ]
-    }
-
-def get_resultado_schema():
-    return {
-        'fields': [
-            {'name': 'nome', 'type': 'STRING'},
-            {'name': 'cpf', 'type': 'INT64'},
-            {'name': 'email', 'type': 'STRING'},
-            {'name': 'DT_CARGA', 'type': 'STRING'}
-        ]
-    }
-
 def run():
+    print("Pipeline execution started")
     options = PipelineOptions()
     p = beam.Pipeline(options=options)
 
@@ -57,39 +27,59 @@ def run():
     ranking_file_path = 'C:/Users/User/OneDrive/Documentos/Luiz Gustavo/Teste Banco ABC/Teste-Banco-ABC/output/ranking_clientes.csv'
     resultado_file_path = 'C:/Users/User/OneDrive/Documentos/Luiz Gustavo/Teste Banco ABC/Teste-Banco-ABC/output/resultado_query.csv'
 
-    # Lê e processa o arquivo ranking_clientes.csv
+    def read_csv(file_path, schema):
+        return (
+            p
+            | f'Read {file_path}' >> beam.io.ReadFromText(file_path, skip_header_lines=1)
+            | f'Parse {file_path}' >> beam.Map(lambda line: dict(zip(schema, next(csv.reader([line])))))
+            | f'Convert Data Types {file_path}' >> beam.Map(lambda row: {
+                col: int(row[col]) if col in ['cpf', 'numeroConta', 'numeroCartao'] and row[col].isdigit() else row[col]
+                for col in row
+            })
+        )
+
+    ranking_schema = ['cpf', 'numeroConta', 'numeroCartao', 'ranking']
+    resultado_schema = ['nome', 'cpf', 'email']
+
+    # Pipeline para dados de ranking_clientes
     ranking_clients = (
-        p
-        | 'Read ranking_clientes.csv' >> beam.io.ReadFromText(ranking_file_path, skip_header_lines=1)
-        | 'Parse ranking_clientes' >> beam.Map(lambda line: dict(zip(['cpf', 'numeroConta', 'numeroCartao', 'ranking'], next(csv.reader([line])))))
+        read_csv(ranking_file_path, ranking_schema)
+        | 'Preprocess Ranking Data' >> beam.Map(preprocess_data)
     )
-
-    # Lê e processa o arquivo resultado_query.csv
-    resultado_query = (
-        p
-        | 'Read resultado_query.csv' >> beam.io.ReadFromText(resultado_file_path, skip_header_lines=1)
-        | 'Parse resultado_query' >> beam.Map(lambda line: dict(zip(['nome', 'cpf', 'email'], next(csv.reader([line])))))
-        | 'Preprocess data' >> beam.Map(preprocess_data)
-    )
-
-    # Salva a tabela ranking_clientes no formato Parquet
+    ranking_schema_arrow = pa.schema([
+        ('cpf', pa.int64()),
+        ('numeroConta', pa.int64()),
+        ('numeroCartao', pa.int64()),
+        ('ranking', pa.string()),
+        ('DT_CARGA', pa.string())
+    ])
     ranking_clients | 'Write ranking to Parquet' >> beam.io.WriteToParquet(
         'output/ranking_clientes.parquet',
-        schema=get_ranking_schema(),
-        file_name_suffix='.parquet',
-        shard_name_template='',
+        schema=ranking_schema_arrow,
+        file_name_suffix='.parquet'
     )
 
-    # Salva a tabela resultado_query no formato Parquet
+    # Pipeline para dados de resultado_query
+    resultado_query = (
+        read_csv(resultado_file_path, resultado_schema)
+        | 'Preprocess Resultado Data' >> beam.Map(preprocess_data)
+    )
+    resultado_schema_arrow = pa.schema([
+        ('nome', pa.string()),
+        ('cpf', pa.int64()),
+        ('email', pa.string()),
+        ('DT_CARGA', pa.string())
+    ])
     resultado_query | 'Write resultado to Parquet' >> beam.io.WriteToParquet(
         'output/resultado_query.parquet',
-        schema=get_resultado_schema(),
-        file_name_suffix='.parquet',
-        shard_name_template='',
+        schema=resultado_schema_arrow,
+        file_name_suffix='.parquet'
     )
 
-    result = p.run()
-    result.wait_until_finish()
+    print("Pipeline step completed")
+    
+    p.run().wait_until_finish()
+    print("Pipeline execution finished")
 
 if __name__ == '__main__':
     run()
