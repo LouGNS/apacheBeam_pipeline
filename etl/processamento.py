9030d452c50d402019dd49a992d2c19ea746123c
Luiz Gustavo <lgustavo.nsilva@gmail.com>
2024-09-11 09:51:11 -0300

    importing necessary libraries to use apache beam

---
+++ etl/processamento.py
@@ -0,0 +1,8 @@
+import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+import unicodedata
+from datetime import datetime
+import pytz
