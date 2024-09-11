028ff7b62e2d1793d419cb0a592437521c75a76f
Luiz Gustavo <lgustavo.nsilva@gmail.com>
2024-09-11 09:53:56 -0300

    Function to remove accentuation and standardize capitalization

--- etl/processamento.py
+++ etl/processamento.py
@@ -7,6 +7,17 @@ import unicodedata
 from datetime import datetime
 import pytz
 
+# Função para remover duplicados
+def remove_duplicates(element):
+    return element[0], element[1]
+
+
+# Função para remover acentuação e padronizar maiúsculas
+def preprocess_string(s):
+    nfkd = unicodedata.normalize('NFKD', s)
+    return u"".join([c for c in nfkd if not unicodedata.combining(c)]).upper().strip()
+
+
 # Função para pré-processar os dados
 def preprocess_data(row):
     row['nome'] = preprocess_string(row['nome'])
